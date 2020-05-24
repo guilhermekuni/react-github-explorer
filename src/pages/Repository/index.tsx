da2f7230ce75073d805e2c531f2e3d44e7946a52
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-24 14:50:34 -0300

    implements Repository styled page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="Rocketseat" />
+          <div>
+            <strong>strong tst</strong>
+            <p>description tst</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1909</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>32</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>43</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="repositories/">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
